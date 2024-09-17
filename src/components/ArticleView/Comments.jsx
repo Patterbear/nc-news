a24c72f14417588deeddfbc6574058b87975572b
Patterbear <benjamin.mcgregor2002@gmail.com>
2024-09-17 09:23:56 +0100

    add no comments handling

--- src/components/ArticleView/Comments.jsx
+++ src/components/ArticleView/Comments.jsx
@@ -2,28 +2,34 @@ import { useState, useEffect } from "react";
 import CommentCard from './CommentCard';
 import { fetchCommentsByArticleId } from "../../../api";
 
-const Comments = ({article_id}) => {
+const Comments = ({ article_id }) => {
     const [comments, setComments] = useState([]);
 
     useEffect(() => {
-        if(article_id) {
+        if (article_id) {
             fetchCommentsByArticleId(article_id)
-            .then((responseComments) => {
-                setComments(responseComments);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+                .then((responseComments) => {
+                    setComments(responseComments);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         }
     }, [article_id]);
 
     return (
         <section>
-            {comments.map((comment) => {
-                return <CommentCard comment={comment} key={comment.comment_id}/>
-            })}
+            {(() => {
+                if (comments.length === 0) {
+                    return <p>No comments yet.</p>;
+                } else {
+                    return comments.map((comment) => (
+                        <CommentCard comment={comment} key={comment.comment_id} />
+                    ));
+                }
+            })()}
         </section>
     );
-}
+};
 
 export default Comments;
