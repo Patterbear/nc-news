cd05a688379c0598adf7f53d652e360aa201fdb4
Patterbear <benjamin.mcgregor2002@gmail.com>
2024-09-17 15:45:35 +0100

    complete task 8

---
+++ src/components/ArticleView/CommentEntry.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { postCommentByArticleId } from '../../../api';
+
+const CommentEntry = ({ article_id, onCommentPosted }) => {
+    const [userComment, setUserComment] = useState('');
+
+    function handlePostComment() {
+        postCommentByArticleId(article_id, {
+            username: 'cooljmessy',
+            body: userComment,
+        })
+        .then(() => {
+            setUserComment('');
+            onCommentPosted();
+        })
+        .catch(err => {
+            console.error('Failed to post comment:', err);
+        });
+    }
+
+    return (
+        <div>
+            <p>Add comment:</p>
+            <input
+                id="comment-entry"
+                value={userComment}
+                onChange={(e) => setUserComment(e.target.value)}
+            />
+            <button onClick={handlePostComment}>Post</button>
+        </div>
+    );
+};
+
+export default CommentEntry;
